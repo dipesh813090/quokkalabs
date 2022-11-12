5b57f57b6aaad0014b307f5bd46c1f73953536d3
dipesh813090 <dipeshkumar813090@gmail.com>
2022-11-12 18:58:51 +0530

    chnages

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.get('/', (req,resp) =>{
     resp.send('welcome page');
 });
-
 app.use('/signup', require('./routes/signup'));
 app.use('/login', require('./routes/login'));
 app.use('/posts', require('./routes/posts'));
